65fbf7715dc732111e560ec36a900464f330e1b9
webmegood <webmegood@gmail.com>
2016-06-08 15:39:19 +1000

    Add files via upload

--- gps.js
+++ gps.js
@@ -1,43 +1,57 @@
-document.addEventListener('deviceready', onDeviceReady, false);
- 
-function onDeviceReady () {
- 
+document.addEventListener('deviceready', setupGeolocation, false);
+
+function setupGeolocation () {
     /**
-    * This callback will be executed every time a geolocation is recorded in the background.
-    */
+     * This function will be executed every time a geolocation was got on the background.
+     */
     var callbackFn = function(location) {
-        console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
- 
-        // Do your HTTP request here to POST location to your server. 
-        // jQuery.post(url, JSON.stringify(location)); 
- 
-        /*
-        IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-        and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-        IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-        */
-        backgroundGeoLocation.finish();
+          // Console.log only is here. You need to setup your own data interaction here
+      console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
+      
+		alert('Latitude: '          + location.latitude          + '\n' +
+              'Longitude: '         + location.longitude         + '\n' +
+              'Altitude: '          + location.altitude          + '\n' +
+              'Accuracy: '          + location.accuracy          + '\n' +
+              'Altitude Accuracy: ' + location.altitudeAccuracy  + '\n' +
+              'Heading: '           + location.heading           + '\n' +
+              'Speed: '             + location.speed             + '\n' +
+              'Timestamp: '         + location.timestamp         + '\n');	  
+	  
+	  /*
+      IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+      and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+      IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+      */
+      backgroundGeoLocation.finish();
     };
- 
+
+      /**
+      * Error handler
+      */
     var failureFn = function(error) {
-        console.log('BackgroundGeoLocation error');
+      console.log('BackgroundGeoLocation error');
+	  
+		alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');	  
+	  
     };
- 
-    // BackgroundGeoLocation is highly configurable. See platform specific configuration options 
+
+    // A lot of options is available here, you can see them all on plugin repo (see link below)
     backgroundGeoLocation.configure(callbackFn, failureFn, {
         desiredAccuracy: 10,
         stationaryRadius: 20,
         distanceFilter: 30,
-        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle. 
-        stopOnTerminate: false, // <-- enable this to clear background location settings when the app terminates 
+        debug: true, // <-- Play sounds for background-geolocation life-cycle. Also will cause local notifications under iOS.
+        stopOnTerminate: false, // <-- Clear background location settings when the app terminates
     });
- 
-    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app. 
+
+    // Start tracking of user coords
     backgroundGeoLocation.start();
- 
-    // If you wish to turn OFF background-tracking, call the #stop method. 
-    // backgroundGeoLocation.stop(); 
+
+    // Stop tracking of user coords
+    // backgroundGeoLocation.stop();
 }
+
 
 
 
