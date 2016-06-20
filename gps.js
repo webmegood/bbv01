369a6a5bbcdc73f34172b3cba563f75347d5d5ec
webmegood <webmegood@gmail.com>
2016-06-20 15:57:38 +1000

    Add files via upload

--- gps.js
+++ gps.js
@@ -39,7 +39,6 @@ function setupGeolocation () {
 		
 		// 3. Add event handler
 		uploadDataButton.addEventListener ("click", function() {
-		  alert(gpsDataArray);
 		  sendtodatabase(gpsDataArray);
 		});
 
@@ -95,7 +94,9 @@ alert(arrayValues);
 
 
 function sendtodatabase(arrayValues){
-	
+
+alert(arrayValues);
+
 $.ajax({
 url: 'http://www.mediathrong.com/beepboards/tracking/scripts/gps_check.php',
 type: 'POST',
