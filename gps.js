61937251e78a7801f78a2b65468caf5b9fccd938
webmegood <webmegood@gmail.com>
2016-06-28 16:17:49 +1000

    Add files via upload

--- gps.js
+++ gps.js
@@ -1,9 +1,9 @@
-document.addEventListener('deviceready', setupGeolocation(), false);
+document.addEventListener('deviceready', setupGeolocation, false);
 
 
 
 
-function setupGeolocation() {
+function setupGeolocation () {
 	
 	
 	
@@ -31,87 +31,6 @@ function setupGeolocation() {
 		
 
 	
-
-    /**
-    * This callback will be executed every time a geolocation is recorded in the background.
-    */
-	
-	var testObject = [];	 // Array containing GPS location objects	 
-
-	var callbackFn = function(location) {
-
-
-		testObject.push([location.latitude, location.longitude, location.time]);
-
-
-
-		// Store
-		if(typeof(window.localStorage) != 'undefined'){ 
-		//var gpsDataArray = JSON.stringify(testObject);
-		var gpsDataArray = JSON.stringify(testObject);
-		localStorage.setItem('testObject', gpsDataArray);
-		//localStorage.setItem('testObject', JSON.stringify(testObject));
-		
-		//send to database once meeting conditions. (if connected, try straight away then wait 60 seconds.)
-		
-		
-
-
-		
-		} else {
-		alert("GPS Data Not Available"); 
-		}
-		
-		
-		// Retrieve 
-		var retrievedObject = localStorage.getItem('testObject');
-		document.getElementById("result").innerHTML = retrievedObject;	
-
-
-        backgroundGeolocation.finish();
-		
-    };
-	
-	
-	
-	
-
-    var failureFn = function(error) {
-        console.log('BackgroundGeolocation error');
-    };
-	
-	
-	
-	
-	
-
-    // BackgroundGeolocation is highly configurable. See platform specific configuration options
-    backgroundGeolocation.configure(callbackFn, failureFn, {
-        desiredAccuracy: 10,
-        stationaryRadius: 10,
-        distanceFilter: 10,
-    	interval: 5000, // <!-- poll for position every 5 secs 
-		//locationService: backgroundGeoLocation.service.ANDROID_FUSED_LOCATION,
-        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-        stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
-    });
-
-    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-    backgroundGeolocation.start();
-
-    // If you wish to turn OFF background-tracking, call the #stop method.
-    // backgroundGeolocation.stop();
-	
-
-
-		// Add event handler to button
-		uploadDataButton.addEventListener ("click", function() {
-			//backgroundGeolocation.stop(); //this will stop the tracking
-		  	//sendtodatabase(gpsDataArray);
-		});
-
-
-
 
 }
 
