23806c0c1e7ce5e6eb9c14330ca40e7631f6722f
webmegood <webmegood@gmail.com>
2016-06-28 15:53:20 +1000

    Add files via upload

--- gps.js
+++ gps.js
@@ -6,29 +6,6 @@ document.addEventListener('deviceready', setupGeolocation(), false);
 function setupGeolocation() {
 	
 	
-	
-		// Remove buttons
-		var uploadElement =  document.getElementById('uploadBtn');
-		if (typeof(uploadElement) != 'undefined' && uploadElement != null)
-		{
-			uploadElement.parentNode.removeChild(uploadElement);
-		}		
-		var restartElement = document.getElementById('restartBtn');
-		if (typeof(restartElement) != 'undefined' && restartElement != null)
-		{
-			restartElement.parentNode.removeChild(restartElement);
-		}		
-				
-		// Create upload button
-		var uploadDataButton = document.createElement("div");
-		uploadDataButton.innerHTML = "Stop + Upload Data";
-		
-		// Append in main content area and append classes and id
-		var positionUploadBtn = document.getElementById('main_content');
-		positionUploadBtn.appendChild(uploadDataButton);
-		uploadDataButton.className = "btn_standard btn_blue";
-		uploadDataButton.setAttribute("id", "uploadBtn");
-		
 
 	
 
@@ -102,7 +79,28 @@ function setupGeolocation() {
     // backgroundGeolocation.stop();
 	
 
-
+		// Remove buttons
+		var uploadElement =  document.getElementById('uploadBtn');
+		if (typeof(uploadElement) != 'undefined' && uploadElement != null)
+		{
+			uploadElement.parentNode.removeChild(uploadElement);
+		}		
+		var restartElement = document.getElementById('restartBtn');
+		if (typeof(restartElement) != 'undefined' && restartElement != null)
+		{
+			restartElement.parentNode.removeChild(restartElement);
+		}		
+				
+		// Create upload button
+		var uploadDataButton = document.createElement("div");
+		uploadDataButton.innerHTML = "Stop + Upload Data";
+		
+		// Append in main content area and append classes and id
+		var positionUploadBtn = document.getElementById('main_content');
+		positionUploadBtn.appendChild(uploadDataButton);
+		uploadDataButton.className = "btn_standard btn_blue";
+		uploadDataButton.setAttribute("id", "uploadBtn");
+		
 		// Add event handler to button
 		uploadDataButton.addEventListener ("click", function() {
 			backgroundGeolocation.stop(); //this will stop the tracking
