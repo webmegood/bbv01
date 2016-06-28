d4d4dd88f11e845b6268861e8733bcdd63a23660
webmegood <webmegood@gmail.com>
2016-06-28 16:40:41 +1000

    Add files via upload

--- gps.js
+++ gps.js
@@ -29,6 +29,11 @@ function setupGeolocation() {
 		uploadDataButton.className = "btn_standard btn_blue";
 		uploadDataButton.setAttribute("id", "uploadBtn");
 		
+		// Add event handler to button
+		uploadDataButton.addEventListener ("click", function() {
+			//backgroundGeolocation.stop(); //this will stop the tracking
+		  	//sendtodatabase(gpsDataArray);
+		});		
 		
 		
 		
@@ -38,6 +43,87 @@ function setupGeolocation() {
 		
 		
 		
+		
+		
+	
+
+    /**
+    * This callback will be executed every time a geolocation is recorded in the background.
+    */
+	
+	var testObject = [];	 // Array containing GPS location objects	 
+
+	var callbackFn = function(location) {
+
+
+		testObject.push([location.latitude, location.longitude, location.time]);
+
+
+
+		// Store
+		if(typeof(window.localStorage) != 'undefined'){ 
+		//var gpsDataArray = JSON.stringify(testObject);
+		var gpsDataArray = JSON.stringify(testObject);
+		localStorage.setItem('testObject', gpsDataArray);
+		//localStorage.setItem('testObject', JSON.stringify(testObject));
+		
+		//send to database once meeting conditions. (if connected, try straight away then wait 60 seconds.)
+		
+		
+
+
+		
+		} else {
+		alert("GPS Data Not Available"); 
+		}
+		
+		
+		// Retrieve 
+		var retrievedObject = localStorage.getItem('testObject');
+		document.getElementById("result").innerHTML = retrievedObject;	
+
+
+       // backgroundGeolocation.finish();
+		
+    };
+	
+	
+	
+	
+
+    var failureFn = function(error) {
+        console.log('BackgroundGeolocation error');
+    };
+	
+	
+	
+	
+	
+
+    // BackgroundGeolocation is highly configurable. See platform specific configuration options
+    backgroundGeolocation.configure(callbackFn, failureFn, {
+        desiredAccuracy: 10,
+        stationaryRadius: 10,
+        distanceFilter: 10,
+    	interval: 5000, // <!-- poll for position every 5 secs 
+		//locationService: backgroundGeoLocation.service.ANDROID_FUSED_LOCATION,
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
+    });
+
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+    backgroundGeolocation.start();
+
+    // If you wish to turn OFF background-tracking, call the #stop method.
+    // backgroundGeolocation.stop();
+	
+
+
+
+
+
+
+
 
 
 
