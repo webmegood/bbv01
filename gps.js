8c4817ceca3c76f9febdea8ef9923bd9a75528c9
webmegood <webmegood@gmail.com>
2016-06-28 16:51:26 +1000

    Add files via upload

--- gps.js
+++ gps.js
@@ -31,8 +31,8 @@ function setupGeolocation() {
 		
 		// Add event handler to button
 		uploadDataButton.addEventListener ("click", function() {
-			//backgroundGeolocation.stop(); //this will stop the tracking
-		  	//sendtodatabase(gpsDataArray);
+			backgroundGeolocation.stop(); //this will stop the tracking
+		  	sendtodatabase(gpsDataArray);
 		});		
 		
 		
@@ -117,6 +117,65 @@ function setupGeolocation() {
     // If you wish to turn OFF background-tracking, call the #stop method.
     // backgroundGeolocation.stop();
 	
+	
+	
+	
+	
+	
+	
+	
+function sendtodatabase(arrayValues){
+
+$.ajax({
+url: 'http://www.mediathrong.com/beepboards/tracking/scripts/gps_check.php',
+type: 'POST',
+data: {data: arrayValues},
+cache: false,
+success: function(output){
+dit = output;
+},
+error: function (request, status, error) {
+}
+});
+
+alert("Your data has been uploaded. Thankyou.");
+
+
+
+createRestartButton();
+
+
+}
+
+
+
+
+
+
+
+
+function createRestartButton() {		
+		
+		// Remove Upload button
+		var uploadElement =  document.getElementById('uploadBtn');
+		uploadElement.parentNode.removeChild(uploadElement);
+		
+		// Create Restart button
+		var restartButton = document.createElement("div");
+		restartButton.innerHTML = "Restart";
+
+		// Append Restart button in main content area and append classes and id
+		var positionRestartBtn = document.getElementById('main_content');
+		positionRestartBtn.appendChild(restartButton);
+		restartButton.className = "btn_standard btn_blue";
+		restartButton.setAttribute("id", "restartBtn");
+
+		// Add event handler
+		restartButton.addEventListener ("click", function() {
+		  	setupGeolocation();
+		});
+		
+}
 
 
 
