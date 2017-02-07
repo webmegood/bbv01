7fe45b70462b45bb4c64b294808610dcb293115f
webmegood <webmegood@gmail.com>
2017-02-07 15:52:51 +1100

    Add files via upload

--- gps03.js
+++ gps03.js
@@ -1,8 +1,12 @@
-
-				
-		// Remove Upload button (if it exists)
+createRestartButton();		
+		
+		
+		
+		
+function createRestartButton() {		
+		// Remove Upload button
 		var uploadElement =  document.getElementById('uploadBtn');
-		if (typeof(element) != 'undefined' && element != null)
+		if (typeof(uploadElement) != 'undefined' && uploadElement != null)
 		{
 			uploadElement.parentNode.removeChild(uploadElement);
 		}		
@@ -17,3 +21,33 @@
 		positionRestartBtn.appendChild(restartButton);
 		restartButton.className = "btn_standard btn_blue";
 		restartButton.setAttribute("id", "restartBtn");
+
+		// Add event handler
+		restartButton.addEventListener ("click", function() {
+		  	createUploadButton();
+		});
+		
+}
+
+
+
+function createUploadButton() {
+		// Remove Restart button
+		var uploadElementRestart = document.getElementById('restartBtn');
+		uploadElementRestart.parentNode.removeChild(uploadElementRestart);
+		
+		// Recreate the upload button
+		var uploadDataButton = document.createElement("div");
+		uploadDataButton.innerHTML = "Stop + Upload Data";
+		
+		// Append in main content area and append classes and id
+		var positionUploadBtn = document.getElementById('main_content');
+		positionUploadBtn.appendChild(uploadDataButton);
+		uploadDataButton.className = "btn_standard btn_blue";
+		uploadDataButton.setAttribute("id", "uploadBtn");
+		
+		// Add event handler
+		uploadDataButton.addEventListener ("click", function() {
+		  	createRestartButton();
+		});
+}
